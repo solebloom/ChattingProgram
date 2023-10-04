b8643cc90e57c1b0685ce6d7e6cfe5d297565564
naramy <dreamdragon7@naver.com>
2023-10-04 19:47:04 +0900

    Style: ChatWriteAttribute class is changed extension target from boost::asio::detail::noncopyable to boost::noncopyable::noncopyable

--- ChatCommon/ChatWriteAttribute.cpp
+++ ChatCommon/ChatWriteAttribute.cpp
@@ -1,8 +1,8 @@
 #pragma once
-#include <boost/noncopyable.hpp>
+#include <boost/core/noncopyable.hpp>
 #include "Session.cpp"
 
-class ChatWriteAttribute : private noncopyable {
+class ChatWriteAttribute : private boost::noncopyable::noncopyable {
 public:
 	virtual void WriteData(Session* session, size_t length) = 0;
 };
