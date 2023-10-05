a844744a26b285b1ddf55544a84b4cc7ea430060
naramy <dreamdragon7@naver.com>
2023-10-06 00:50:50 +0900

    Fix: initialization standard error about std::atomic_flag in BackOffSpinLock header

--- ChatCommon/SpinLock/BackOffSpinLock.hpp
+++ ChatCommon/SpinLock/BackOffSpinLock.hpp
@@ -13,7 +13,11 @@ public:
 private:
     int getRandomDelay(int currentDelay) noexcept;
     
+#if defined _HAS_CXX20 && _HAS_CXX20 > 0
     std::atomic_flag lockFlag{};
+#else
+    std::atomic_flag lockFlag = ATOMIC_FLAG_INIT;
+#endif
     const int minDelay;
     const int maxDelay;
     int currentDelay;
